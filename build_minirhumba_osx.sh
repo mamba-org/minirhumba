3bf7065fcc5243192951bb760f354606c8cf408f
Mariana Meireles <mariana@psychonautgirl.space>
2020-09-07 16:27:50 +0200

    Adding conda again

--- build_minirhumba_osx.sh
+++ build_minirhumba_osx.sh
@@ -3,12 +3,15 @@
 set -e
 set -x
 
-echo "Installing a fresh version of rhumba."
+echo "Installing a fresh version of Miniforge3 to build the rhumba environment."
 MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/download/4.8.3-1"
 MINIFORGE_FILE="Miniforge3-4.8.3-1-MacOSX-x86_64.sh"
 
 curl -L -O "${MINIFORGE_URL}/${MINIFORGE_FILE}"
 bash $MINIFORGE_FILE -b
+
+echo "Configuring conda."
+source ~/miniforge3/bin/activate root
 
 export CONSTRUCT_ROOT=$PWD
 mkdir -p build
