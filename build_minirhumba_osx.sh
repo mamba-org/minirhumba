6b5ec247bfa876de53faba9085afd48b56f267d9
Mariana Meireles <mariana@psychonautgirl.space>
2020-09-07 15:58:57 +0200

    Fix from tag to download typo

--- build_minirhumba_osx.sh
+++ build_minirhumba_osx.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 echo "Installing a fresh version of rhumba."
-MINIRHUMBA_URL="https://github.com/TheSnakePit/minirhumba/releases/tag/0.0.1"
+MINIRHUMBA_URL="https://github.com/TheSnakePit/minirhumba/releases/download/0.0.1"
 MINIRHUMBA_FILE="minirhumba-0.0.1-MacOSX-x86_64.sh"
 
 curl -L -O "${MINIRHUMBA_URL}/${MINIRHUMBA_FILE}"
