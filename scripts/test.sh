a468fbe9fff61f28dc667bea9b14554ec7e544a9
Mariana Meireles <mariana@psychonautgirl.space>
2020-09-07 18:30:59 +0200

    Removing more tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -17,24 +17,4 @@ echo "***** Run the installer *****"
 chmod +x $INSTALLER_PATH
 bash $INSTALLER_PATH -b -p $RHUMBA_PATH
 
-echo "***** Setup conda *****"
-source $CONDA_PATH/bin/activate
-
-echo "***** Print conda info *****"
-conda info
-
-echo "***** Run conda update *****"
-conda update --all -y
-
-echo "***** Python path *****"
-python -c "import sys; print(sys.executable)"
-python -c "import sys; assert 'minirhumba' in sys.executable"
-
-echo "***** Print system informations from Python *****"
-python -c "print('Hello minirhumba !')"
-python -c "import platform; print(platform.architecture())"
-python -c "import platform; print(platform.system())"
-python -c "import platform; print(platform.machine())"
-python -c "import platform; print(platform.release())"
-
 echo "***** Done: Building Testing installer *****"
