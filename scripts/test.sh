1662047cca7745c92b743a5582f90bd03d9d32df
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-05-18 10:03:12 +0200

    add small test

--- scripts/test.sh
+++ scripts/test.sh
@@ -24,8 +24,20 @@ chmod +x $INSTALLER_PATH
 if [[ "$(uname)" == MINGW* ]]; then
   echo "start /wait \"\" ${INSTALLER_PATH} /InstallationType=JustMe /RegisterPython=0 /S /D=$(cygpath -w $RHUMBA_PATH)" > install.bat
   cmd.exe /c install.bat
+  source $CONDA_PATH/Scripts/activate
+
+  Rscript -e "rhumba::set_config('auto_yes', 'true'); rhumba::install('xtensor')"
+
+  test -d $CONDA_PATH/Library/include/xtensor
+  test -f $CONDA_PATH/Library/include/xtensor/xtensor.hpp
 else
   bash $INSTALLER_PATH -b -p $RHUMBA_PATH
+  source $CONDA_PATH/bin/activate
+
+  Rscript -e "rhumba::set_config('auto_yes', 'true'); rhumba::install('xtensor')"
+
+  test -d $CONDA_PATH/include/xtensor
+  test -f $CONDA_PATH/include/xtensor/xtensor.hpp
 fi
 
 echo "***** Done: Testing installer *****"
